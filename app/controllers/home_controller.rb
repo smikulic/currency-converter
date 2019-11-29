fe8bc71846a00c2de64f2733afde4494c75f0236
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2019-11-30 00:47:54 +0100

    Optimise exchange rate initialise method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,25 +1,21 @@
 require 'money/bank/open_exchange_rates_bank'
 
 class HomeController < ApplicationController
+  def initialize
+    super # this calls ActionController::Base initialize
+
+    @oxr = Money::Bank::OpenExchangeRatesBank.new
+    @oxr.cache = './exchange_rates_cache.json'    
+    @oxr.app_id = 'a52b48990d0a4081b3354e803d5bf633' # This would usually go into ENV variables or secrets
+    @oxr.update_rates
+    @oxr.source = 'USD' # OpenexchangeratesBank only allows USD as base currency for the free plan users
+  end
 
   def calculate_exchange
-    oxr = Money::Bank::OpenExchangeRatesBank.new
-    oxr.cache = './exchange_rates_cache.json'
-    
-    # This would usually go into ENV variables or secrets.
-    oxr.app_id = 'a52b48990d0a4081b3354e803d5bf633'
-    oxr.update_rates
-    oxr.date = params[:date]
+    @oxr.date = params[:date]
+    @oxr.save_rates # Store in cache
 
-    # OpenexchangeratesBank only allows USD as base currency for the free plan users.
-    oxr.source = 'USD'
-
-    # Store in cache
-    oxr.save_rates
-
-    Money.default_bank = oxr
-    @exchange = Money.default_bank.get_rate(params[:exchangeFrom], params[:exchangeTo])
-
-    render :json => @exchange
+    Money.default_bank = @oxr
+    render :json => Money.default_bank.get_rate(params[:exchangeFrom], params[:exchangeTo])
   end
 end
